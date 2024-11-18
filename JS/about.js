268d8b9491f2890022d4b36034c0d77b84dad31d
AnthonyGordonbit <ajgordona@gmail.com>
2024-11-17 17:31:18 -0700

    Linking JS

---
+++ JS/about.js
@@ -0,0 +1,3 @@
+console.log("JavaScript is working!");
+alert("JavaScript is linked and functional!");
+document.body.style.backgroundColor = "lightblue"; // Changes the background color to confirm it works.
